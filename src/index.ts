210d04092da14a0ec17f1ddc071ffcf98206d43f
soulwax <soulwax-95@protonmail.com>
2026-03-06 00:07:21 +0100

    Add AI interface and bindings to worker configuration

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,18 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+export interface Env {
+	// If you set another name in the Wrangler config file as the value for 'binding',
+	// replace "AI" with the variable name you defined.
+	AI: Ai;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		const response = await env.AI.run('@cf/meta/llama-3.1-8b-instruct' as any, {
+			prompt: 'What is the origin of the phrase Hello, World',
+		});
+
+		return new Response(JSON.stringify(response));
 	},
 } satisfies ExportedHandler<Env>;
